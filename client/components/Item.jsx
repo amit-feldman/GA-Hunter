be341d21bdb4b92b60006b10eb853b1608dd64a2
tom <thomasjohnmulroy@gmail.com>
2016-10-09 19:06:50 -0400

    render ItemList component from index.jsx

--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemView from './ItemView.jsx';
+
 const Item = () => {
   return (
     <ItemView />
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './components/App.jsx';
-import ModalView from './components/Modal.jsx';
+// import ModalView from './components/Modal.jsx';
+import ItemList from './components/ItemList.jsx';
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
-ReactDOM.render(<ModalView />, document.querySelector('#root'));
+// ReactDOM.render(<ModalView />, document.querySelector('#root'));
+ReactDOM.render(<ItemList />, document.querySelector('#root'));
