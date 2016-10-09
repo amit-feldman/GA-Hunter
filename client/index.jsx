97e2e6e7b1b87934251acde6fe8fa4bd539c0237
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-08 20:05:16 -0400

    finish dummy version of modal view.  Comment out App in the index.jsx to test ModalView in that file

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+// import App from './components/App.jsx';
+import ModalView from './components/Modal.jsx';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<ModalView />, document.querySelector('#root'));
