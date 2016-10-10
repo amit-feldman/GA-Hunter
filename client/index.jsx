b90e544bc92230ca4e5c016623ebc3bd6cad64e0
tom <thomasjohnmulroy@gmail.com>
2016-10-10 14:46:36 -0400

    implement dummy component for CommentList and import it into index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 // import App from './components/App.jsx';
 // import ModalView from './components/Modal.jsx';
 // import ItemList from './components/ItemList.jsx';
-import Profile from './components/Profile.jsx';
+// import Profile from './components/Profile.jsx';
+import CommentList from './components/CommentList.jsx';
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
 // ReactDOM.render(<ModalView />, document.querySelector('#root'));
 // ReactDOM.render(<ItemList />, document.querySelector('#root'));
-ReactDOM.render(<Profile />, document.querySelector('#root'));
+// ReactDOM.render(<Profile />, document.querySelector('#root'));
+ReactDOM.render(<CommentList />, document.querySelector('#root'));
