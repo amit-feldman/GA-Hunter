d82e684e1a15a3d3c52f297cffd0c8886a945c11
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-11 17:30:19 -0400

    fixes a hiccup in the findBy method for the ProjectDAO

--- app/services/ProjectDAO.js
+++ app/services/ProjectDAO.js
@@ -8,7 +8,7 @@ class ProjectDAO {
   }
 
   static findBy(keyValue) {
-    const key = Object.keys(keyValue)[1];
+    const key = Object.keys(keyValue)[0];
     const value = keyValue[key];
     return db
     .one(sql.find, [key, value])
