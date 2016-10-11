d0e34311e9928444158c68dffc4e55e50c59202c
Amit Feldman <amit@feldman.cl>
2016-10-11 11:19:07 -0400

    add createdAt to user model

--- app/config/sql/user/create.sql
+++ app/config/sql/user/create.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (email, password, course) VALUES($1, $2, $3) RETURNING *;
+INSERT INTO users (email, password, course, createdAt) VALUES($1, $2, $3, $4) RETURNING *;
--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,10 @@
 class User {
-  constructor({ id, email, password, course }) {
+  constructor({ id, email, password, course, createdAt }) {
     this.id = id;
     this.email = email;
     this.password = password;
     this.course = course;
+    this.createdAt = createdAt;
   }
 }
 
