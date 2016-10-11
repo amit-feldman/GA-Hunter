8c31e37b6029468b249f4d3ba1111a1365e46537
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-11 17:22:43 -0400

    add a ProjectDAO and fix spelling error in sqlProvider

---
+++ app/services/ProjectDAO.js
@@ -0,0 +1,29 @@
+const db = require('../config/db');
+const sql = require('../config/sqlProvider').projects;
+const Project = require('../models/Project');
+
+class ProjectDAO {
+  static all() {
+    return db.map(sql.all, [], (row) => new Project(row));
+  }
+
+  static findBy(keyValue) {
+    const key = Object.keys(keyValue)[1];
+    const value = keyValue[key];
+    return db
+    .one(sql.find, [key, value])
+    .then((row) => new Project(row));
+  }
+
+  static create({ name, description, image, project, upvote }) {
+    return db
+    .one(sql.create, [name, description, image, project, upvote])
+    .then((row) => new Project(row));
+  }
+
+  static delete(id) {
+    return db.none(sql.delete, [id]);
+  }
+}
+
+module.exports = ProjectDAO;
