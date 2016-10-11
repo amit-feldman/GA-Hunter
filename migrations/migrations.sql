24490d71a75d1b5ae2421ab29aaba6d5f95db11a
tom <thomasjohnmulroy@gmail.com>
2016-10-11 11:29:55 -0400

    create a projects table migration

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -10,5 +10,16 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR NOT NULL,
   password VARCHAR NOT NULL,
-  course VARCHAR NOT NULL
+  course VARCHAR NOT NULL,
+  created_at DATETIME
+);
+
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(128),
+  description TEXT,
+  img_url VARCHAR(255),
+  project_url VARCHAR(255),
+  upvote_count INTEGER,
+  created_at DATETIME
 );
