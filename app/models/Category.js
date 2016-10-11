89b261bdc1bd3d7a9d10c427742b03c4d0f57ad6
tom <thomasjohnmulroy@gmail.com>
2016-10-11 15:30:05 -0400

    create Category model and Comment model

---
+++ app/models/Category.js
@@ -0,0 +1,10 @@
+class Category {
+  constructor({ id, name, project_id, created }) {
+    this.id = id;
+    this.name = name;
+    this.project_id = project_id;
+    this.created = created;
+  }
+}
+
+module.exports = Category;
