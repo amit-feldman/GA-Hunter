1b13cdc04a33894f99a2c72ab3597a690cbb0459
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-11 12:23:57 -0400

    add projects seed information

---
+++ seed/projects.sql
@@ -0,0 +1,30 @@
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('tictactoe', 'a fun js game', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 10, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('dice game', 'roll the dice', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 12, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('t rex chase', 'dinosaur tech', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 9, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('space invader', 'alien warfare', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 8, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('funtimes', 'have fun', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 7, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('movie search', 'find films', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 9, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('nonsense time', 'absurdist fun', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 16, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('cookie recipes', 'bake your heart out', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 16, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('gamify math', 'learn subtraction', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 13, '1901-01-02 01:00:00');
+INSERT INTO
+  projects (name, description, image, project, upvote, created)
+  VALUES ('personal brand', 'make a personal statement', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 12, '1901-01-02 01:00:00');
