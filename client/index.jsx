edfbf2f6bf1cc0bc531e50a972d46586464bbb5d
tom <thomasjohnmulroy@gmail.com>
2016-10-11 10:23:39 -0400

    render Upvotes component within index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 // import ModalView from './components/Modal.jsx';
 // import ItemList from './components/ItemList.jsx';
 // import Profile from './components/Profile.jsx';
-import CommentList from './components/CommentList.jsx';
+// import CommentList from './components/CommentList.jsx';
+import Upvotes from './components/Upvotes.jsx';
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
 // ReactDOM.render(<ModalView />, document.querySelector('#root'));
 // ReactDOM.render(<ItemList />, document.querySelector('#root'));
 // ReactDOM.render(<Profile />, document.querySelector('#root'));
-ReactDOM.render(<CommentList />, document.querySelector('#root'));
+// ReactDOM.render(<CommentList />, document.querySelector('#root'));
+ReactDOM.render(<Upvotes />, document.querySelector('#root'));
