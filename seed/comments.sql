6b220eb7e0f5dbb3c4a4a48e05d2ca48f70650b1
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-11 14:01:12 -0400

    make a seed file for intitial comments table

---
+++ seed/comments.sql
@@ -0,0 +1,26 @@
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('great work.  Id love to see the final product', 3, 8);\
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('How did you get it to do that animation in phases like that?', 4, 2);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('you should check out this documentation for refactoring code', 1, 6);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('You need to refactor', 6, 9);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('what a cool project guys, you nailed it', 5, 5);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('lets get together and work on something soon.  Id love to work on something like that with you', 8, 9);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Cool original idea, but I think your styling needs some work', 2, 1);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('A great project, youre doing GA proud', 3, 4);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Love how DRY the code is', 8, 10);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Feels good, feels great', 10, 8);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Hey guys, whats going on with GIT right now', 2, 7);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Background should be lightblue', 4, 6);
+INSERT INTO comments (body, user_id, project_id)
+  VALUES ('Nice way to integrate React components into a project that is heavy on back end work', 5, 1);
