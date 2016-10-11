a1bc0e97d6e7aaec3d7aa7b68d1197342d49be7c
tom <thomasjohnmulroy@gmail.com>
2016-10-11 12:04:31 -0400

    create migration for upvotes

--- migrations/migrations.sql
+++ migrations/migrations.sql
@@ -45,3 +45,10 @@ CREATE TABLE categories (
   project_id INTEGER REFERENCES projects (id),
   created TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE upvotes (
+  id SERIAL PRIMARY KEY,
+  project_id INTEGER REFERENCES projects (id),
+  user_id INTEGER REFERENCES users (id),
+  created TIMESTAMP NOT NULL DEFAULT NOW()
+);
