f8fb38eaa790838de0f8080a9d90e5fb63b8789e
Amit Feldman <icsvortex666@users.noreply.github.com>
2016-10-12 17:26:05 -0400

    Update ProjectController.js

--- app/controllers/ProjectController.js
+++ app/controllers/ProjectController.js
@@ -3,26 +3,19 @@ const ProjectDAO = require('../services/ProjectDAO');
 
 class ProjectController {
   static getAll(req, res) {
-    ProjectDAO.all().then((projects) => {
+    ProjectDAO.all()
+    .then((projects) => {
       res.status(200).json(projects);
     });
   }
 
   static getOne(req, res) {
-<<<<<<< HEAD
-    ProjectDAO
-    .findBy({
-      id: req.params.id,
-    })
-    .then((project) => res.status(200).json(project));
-=======
     ProjectDAO.findBy({
       id: req.params.id,
     })
     .then((project) => {
       res.status(200).json(project);
     });
->>>>>>> b1470c4e0ce844d697c4702615dc024e2742f57b
   }
 
   static createProject(req, res) {
@@ -45,11 +38,7 @@ class ProjectController {
 
   static deleteProject(req, res) {
     const id = req.params.id;
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> b1470c4e0ce844d697c4702615dc024e2742f57b
     ProjectDAO.delete(id)
     .then(() => {
       res.status(204).send();
