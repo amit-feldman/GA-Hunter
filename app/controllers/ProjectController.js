33b39f5c4abb1eca8bab1b7e92e6086968103bd1
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-12 16:42:51 -0400

    fixes small style erros in Project Controller and sets up Upvote Controller

--- app/controllers/ProjectController.js
+++ app/controllers/ProjectController.js
@@ -10,26 +10,30 @@ class ProjectController {
   static getOne(req, res) {
     ProjectDAO
     .findBy({ id: req.params.id })
-    .then((project) => res.status(200).json(project));
+    .then((project) => {
+      res.status(200).json(project);
+    });
   }
   static createProject(req, res) {
     const projectData = req.body;
     ProjectDAO.create(projectData)
     .then((project) => {
-      res.status(200).send(project);
+      res.status(200).json(project);
     });
   }
   static updateProject(req, res) {
     const projectData = req.body;
     ProjectDAO.update(projectData)
     .then((project) => {
-      res.status(200).send(project);
+      res.status(200).json(project);
     });
   }
   static deleteProject(req, res) {
     const id = req.params.id;
     ProjectDAO.delete(id)
-    .then(() => res.status(204).send());
+    .then(() => {
+      res.status(204).send();
+    });
   }
 }
 
