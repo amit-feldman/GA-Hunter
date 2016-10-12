ad792c2445380f8800e7d2edddc2e3c2614dfe91
Amit Feldman <icsvortex666@users.noreply.github.com>
2016-10-12 17:09:08 -0400

    Update UpvoteDAO.js

--- app/services/UpvoteDAO.js
+++ app/services/UpvoteDAO.js
@@ -10,15 +10,18 @@ class UpvoteDAO {
   static findBy(keyValue) {
     const key = Object.keys(keyValue)[0];
     const value = keyValue[key];
+
     return db
     .one(sql.find, [key, value])
     .then((row) => new Upvote(row));
   }
+
   static create({ project_id, user_id, created }) {
     return db
     .one(sql.create, [project_id, user_id, created])
     .then((row) => new Upvote(row));
   }
+  
   static delete(id) {
     return db.none(sql.delete, [id]);
   }
