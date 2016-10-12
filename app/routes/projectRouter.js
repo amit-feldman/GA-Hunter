9e0ed35917dea299b0ed1866f1a50bc7610681f5
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-12 15:31:43 -0400

    set up initial routing in projectRouter

---
+++ app/routes/projectRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const ProjectController = require('../controllers/ProjectController');
+
+const router = express.Router();
+
+router.get('/', ProjectController.getAll);
+router.get('/:id', ProjectController.getOne);
+router.post('/', ProjectController.createProject);
+router.patch('/:id', ProjectController.updateProject);
+router.delete('/:id', ProjectController.deleteProject);
