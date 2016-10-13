3449f7b90ac9db7ba6690aa9215393babe8c10e3
tom <thomasjohnmulroy@gmail.com>
2016-10-13 15:07:32 -0400

    redirect a non-logged in user to the login form when they click new project

--- client/utils/requireAuth.js
+++ client/utils/requireAuth.js
@@ -4,34 +4,14 @@ function requireAuth(nextState, replace, callback) {
   const path = `api${nextState.location.pathname}`;
   request.get(path)
     .then(res => {
-      console.log('in .then auth')
       callback();
     }).catch((err) => {
-      console.log(err)
       replace({
         pathname: '/login',
         state: {next: nextState.location.pathname}
       });
+      callback();
     })
 };
 
 export default requireAuth;
-
-
-
-// import request from 'superagent';
-//
-// function requireAuth(nextState, replace, callback) {
-//   const path = `api${nextState.location.pathname}`;
-//   request.get(path)
-//     .then(res => {
-//       replace(path);
-//       callback();
-//     })
-//     .catch((error) => {
-//       replace('/login');
-//       callback(error);
-//     });
-// };
-//
-// export default requireAuth;
