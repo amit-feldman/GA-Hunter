16aaad0586689a0c2d2575c1bc02d2375911cad7
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-13 19:06:39 -0400

    add the intial ability for the upvote view to insert into the upvotes table of the database.

--- client/components/UpvoteView.jsx
+++ client/components/UpvoteView.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 
 const propTypes = {
   user_id: React.PropTypes.number,
@@ -8,13 +9,34 @@ const propTypes = {
 class UpvoteView extends Component {
   constructor(props) {
     super(props);
-    this.user_id = this.props.user_id;
-    this.project_id = this.props.project_id;
+
+    this.state = {
+      user_id: this.props.user_id,
+      project_id: this.props.project_id,
+    };
+    this.handleUpvote = this.handleUpvote.bind(this);
   }
-  
+  handleUpvote(e) {
+    e.preventDefault();
+
+    const apiUrl = 'http://localhost:3000/api/upvotes';
+    const { user_id, project_id } = this.state;
+
+    request.post(apiUrl)
+    .send({
+      user_id: user_id,
+      project_id: project_id,
+    })
+    .end(() => {
+      console.log('outcome of request');
+    })
+  }
   render() {
     return (
-      <button className="btn btn-xs btn-upvote">
+      <button
+        className="btn btn-xs btn-upvote"
+        onClick={this.handleUpvote}
+      >
         <svg width="9" height="8" viewBox="0 0 9 8" xmlns="http://www.w3.org/2000/svg">
           <title>Like</title>
           <path d="M9 8H0l4.5-8L9 8z" fillRule="evenodd" />
