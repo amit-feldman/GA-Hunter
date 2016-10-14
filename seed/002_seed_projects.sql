e81eb19459e5ebe024e0cacde3d13bc4c6cb1e12
Mark Robertson <markrivasrobertson@yahoo.com>
2016-10-14 07:00:12 -0400

    small fix to seed file, adding needed comma

--- seed/002_seed_projects.sql
+++ seed/002_seed_projects.sql
@@ -4,31 +4,31 @@ TRUNCATE TABLE projects;
 
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('tictactoe', 'a fun js game', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 10, 1);
+  VALUES ('tictactoe', 'a fun js game', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 10, 1);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('dice game', 'roll the dice', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 12, 2);
+  VALUES ('dice game', 'roll the dice', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 12, 2);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('t rex chase', 'dinosaur tech', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 9, 3);
+  VALUES ('t rex chase', 'dinosaur tech', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 9, 3);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('space invader', 'alien warfare', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 8, 4);
+  VALUES ('space invader', 'alien warfare', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 8, 4);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('funtimes', 'have fun', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 7, 5);
+  VALUES ('funtimes', 'have fun', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 7, 5);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('movie search', 'find films', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 9, 6);
+  VALUES ('movie search', 'find films', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 9, 6);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('nonsense time', 'absurdist fun', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 16, 7);
+  VALUES ('nonsense time', 'absurdist fun', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 16, 7);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('cookie recipes', 'bake your heart out', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 16, 8);
+  VALUES ('cookie recipes', 'bake your heart out', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 16, 8);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('gamify math', 'learn subtraction', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 13, 9);
+  VALUES ('gamify math', 'learn subtraction', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 13, 9);
 INSERT INTO
   projects (name, description, tagline, image, url, banner, upvote, user_id)
-  VALUES ('personal brand', 'make a personal statement', 'yummy tagline' 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 12, 10);
+  VALUES ('personal brand', 'make a personal statement', 'yummy tagline', 'http://www.fillmurray.com/200/200', 'www.yahoo.com', 'http://placehold.it/1000x400', 12, 10);
